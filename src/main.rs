14b9091365323f20a480b7ffcd93b8c51097d53d
Boris-Chengbiao Zhou <bobo1239@web.de>
2019-11-24 00:34:27 +0100

    Update for vst 0.2.0

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-extern crate vst;
-extern crate hound;
-
-use std::sync::{Arc, Mutex};
-use std::path::PathBuf;
 use std::error::Error;
+use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
 
-use vst::host::{Host, PluginLoader};
-use vst::plugin::Plugin;
-use vst::event::MidiEvent;
-use vst::buffer::{AudioBuffer, SendEventBuffer};
+use hound::{SampleFormat, WavSpec, WavWriter};
 use vst::api::Events;
+use vst::buffer::SendEventBuffer;
+use vst::event::MidiEvent;
+use vst::host::{Host, HostBuffer, PluginLoader};
+use vst::plugin::Plugin;
 
 const BUFFER_SIZE: usize = 1024;
 const SAMPLE_RATE: u32 = 44_100;
@@ -17,7 +15,7 @@ const SAMPLE_RATE: u32 = 44_100;
 struct SampleHost;
 
 impl Host for SampleHost {
-    fn automate(&mut self, _index: i32, _value: f32) {
+    fn automate(&self, _index: i32, _value: f32) {
         unimplemented!();
     }
 
@@ -34,7 +32,7 @@ impl Host for SampleHost {
         (0, "Hello".into(), "World".into())
     }
 
-    fn process_events(&mut self, _events: &Events) {
+    fn process_events(&self, _events: &Events) {
         println!("Host: process_events");
     }
 }
@@ -43,60 +41,38 @@ fn main() {
     let path = if let Some(path) = std::env::args().nth(1) {
         PathBuf::from(path)
     } else {
-        PathBuf::from(
-            "D:\\Program Files\\Common Files\\VST2\\Pianoteq 5 (64-bit).dll",
-        )
+        PathBuf::from("D:\\Program Files\\Common Files\\VST2\\Pianoteq 6 (64-bit).dll")
     };
 
-    let spec = hound::WavSpec {
+    let spec = WavSpec {
         channels: 2,
         sample_rate: SAMPLE_RATE,
         bits_per_sample: 16,
-        sample_format: hound::SampleFormat::Int,
+        sample_format: SampleFormat::Int,
     };
-    let mut wav_writer = hound::WavWriter::create("out.wav", spec).unwrap();
+    let mut wav_writer = WavWriter::create("out.wav", spec).unwrap();
 
     let host = Arc::new(Mutex::new(SampleHost));
 
-    println!("Loading {}...", path.to_str().unwrap());
+    println!("Loading {}...", path.display());
 
-    let mut loader = PluginLoader::load(&path, host.clone())
+    let mut loader = PluginLoader::load(&path, host)
         .unwrap_or_else(|e| panic!("Failed to load plugin: {}", e.description()));
 
-    let mut instance = loader.instance().unwrap();
-    let info = instance.get_info();
+    let mut plugin = loader.instance().unwrap();
+    let info = plugin.get_info();
     println!("{:#?}", info);
 
-    instance.init();
-    instance.set_sample_rate(SAMPLE_RATE as f32);
-
-    let mut input_buffers = Vec::new();
-    let mut output_buffers = Vec::new();
-    for _ in 0..info.inputs {
-        input_buffers.push([0.0f32; BUFFER_SIZE]);
-    }
-    for _ in 0..info.outputs {
-        output_buffers.push([0.0f32; BUFFER_SIZE]);
-    }
-
-    let mut input_pointers = Vec::new();
-    for a in input_buffers {
-        input_pointers.push(a.as_ptr());
-    }
-
-    let mut output_pointers = Vec::new();
-    for out in &mut output_buffers {
-        output_pointers.push(out.as_mut_ptr());
-    }
+    plugin.init();
+    plugin.set_sample_rate(SAMPLE_RATE as f32);
 
     // let notes = [60, 62, 64, 65, 67, 69, 71, 72]; // C major scale
     let notes = [74, 76, 78, 79, 81, 83, 85, 86]; // D major scale
 
     let mut midi_events = Vec::new();
-    let mut send_buffers = Vec::new();
     for note in &notes {
         midi_events.push(MidiEvent {
-            data: [144, *note, 90],
+            data: [0x90, *note, 90], // Note on
             delta_frames: 0,
             live: false,
             note_length: None,
@@ -104,22 +80,20 @@ fn main() {
             detune: 0,
             note_off_velocity: 0,
         });
-        let mut send_buffer = SendEventBuffer::new(1);
-        send_buffer.store_midi(&[midi_events[midi_events.len() - 1]]);
-        send_buffers.push(send_buffer);
     }
 
-    let mut buffer = AudioBuffer::new(
-        input_pointers.as_slice(),
-        output_pointers.as_mut_slice(),
-        BUFFER_SIZE,
-    );
+    let mut host_buffer: HostBuffer<f32> = HostBuffer::from_info(&info);
+    let inputs = vec![vec![0.0; BUFFER_SIZE]; host_buffer.input_count()];
+    let mut outputs = vec![vec![0.0; BUFFER_SIZE]; host_buffer.output_count()];
+    let mut audio_buffer = host_buffer.bind(&inputs, &mut outputs);
+
+    let mut send_buffer = SendEventBuffer::new(1);
 
     let mut k = 0;
     let mut direction = true;
     for i in 0..500 {
         if i % 10 == 0 {
-            instance.process_events(send_buffers[k].events());
+            send_buffer.send_events_to_plugin(&[midi_events[k]], &mut plugin);
 
             if k == 0 {
                 direction = true;
@@ -134,9 +108,9 @@ fn main() {
             }
         }
 
-        instance.process(&mut buffer);
+        plugin.process(&mut audio_buffer);
 
-        let (_inputs, outputs) = buffer.split();
+        let (_inputs, outputs) = audio_buffer.split();
         for s in outputs[0].iter().zip(outputs[1].iter()) {
             wav_writer
                 .write_sample((*s.0 * std::i16::MAX as f32) as i16)
